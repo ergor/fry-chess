507aedbb6393aba2e92363f350578a2e2d4ac6a8
Tom Kong <tom.kong@visma.com>
2019-10-18 11:26:08 +0200

    main loop ish

--- src/main/java/st/netb/chess/fry/Fry.java
+++ src/main/java/st/netb/chess/fry/Fry.java
@@ -1,6 +1,25 @@
 
 package st.netb.chess.fry;
 
+import java.util.Scanner;
+
 public class Fry {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System. in);
+		while(true) {
+			//Board board = new Board();
+			System.out.println("X to Move: ");
+			String inputMove = scanner.nextLine();
+			if(inputMove.toLowerCase().equals("exit")) break;
+			doMove(inputMove);
+		}
+
+
+	}
+
+	private static void doMove(String input) {
+
+	}
 
 }
