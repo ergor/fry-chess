353af5acef1866d579b9dce0a6bed5171420ec1d
Tom Kong <tom.kong@visma.com>
2019-10-18 11:38:32 +0200

    main loop ish

--- src/main/java/st/netb/chess/fry/Fry.java
+++ src/main/java/st/netb/chess/fry/Fry.java
@@ -1,15 +1,25 @@
 
 package st.netb.chess.fry;
 
+import st.netb.chess.lib.Board;
+
 import java.util.Scanner;
 
 public class Fry {
+	public static Board board;
+
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System. in);
+		board = Board.getStartingBoard();
 		while(true) {
-			//Board board = new Board();
-			System.out.println("X to Move: ");
+			board.toString();
+			Board.Turn turn = board.getTurn();
+			if(turn == Board.Turn.WHITE) {
+				System.out.println("White to Move: ");
+			} else {
+				System.out.println("Black to Move: ");
+			}
 			String inputMove = scanner.nextLine();
 			if(inputMove.toLowerCase().equals("exit")) break;
 			doMove(inputMove);
@@ -19,7 +29,8 @@ public class Fry {
 	}
 
 	private static void doMove(String input) {
-
+		// stuff;
+		board = board;
 	}
 
 }
