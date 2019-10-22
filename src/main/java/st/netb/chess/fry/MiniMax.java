eb08ba29bfbb543f60c74e18c5df94c66cb58109
stian.tosse@tripletex.no <stian.tosse@tripletex.no>
2019-10-22 20:09:38 +0200

    minmax stuff

--- src/main/java/st/netb/chess/fry/MiniMax.java
+++ src/main/java/st/netb/chess/fry/MiniMax.java
@@ -6,8 +6,9 @@ import java.util.List;
 
 class MiniMax{
 
-    private static int depth = 5;
+    private static int depth = 3;
 
+    private static int searchCount = 0;
 
      public static Board getGoodMove(Board board){
         List<Board> boards = BoardGenerator.getBoards(board);
@@ -15,21 +16,59 @@ class MiniMax{
         int scoreBestBoard = (board.getTurn() == Piece.Color.WHITE) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
 
         for(Board b: boards) {
-            int val = minimax(b, depth, Integer.MIN_VALUE, Integer.MIN_VALUE, b.getTurn() == Piece.Color.WHITE);
+            int val = minimax(b, depth, Integer.MIN_VALUE, Integer.MAX_VALUE, b.getTurn() == Piece.Color.WHITE);
             if(isBest(val, scoreBestBoard, b.getTurn() == Piece.Color.WHITE)){
                 scoreBestBoard = val;
                 bestMove = b;
                 bestMove.setScore(scoreBestBoard);
             };
         }
+        System.out.println(searchCount);
+        System.out.println(scoreBestBoard);
         return bestMove;
     }
     private static boolean isBest(int value, int compareTo, boolean isWhite){
-        return (isWhite) ? value > compareTo : value < compareTo;
+        return (isWhite) ? value < compareTo : value > compareTo;
     }
 
 
+    private static int minimax2(Board board, int depth, int alpha, int beta, boolean isMax){
+        searchCount++;
+        int value;
+        List<Board> boards = BoardGenerator.getBoards(board);
+        if(depth == 0 || boards.size() == 0){//or node is terminal
+            return Evaluator.evaluateBoard(board);
+        }
+        if(isMax){
+            value = Integer.MIN_VALUE;
+            for(Board childBoard: boards){
+                int newValue = minimax(childBoard, depth -1, alpha, beta, false);
+                value = Math.max(newValue, value);
+
+                alpha = Math.max(value, alpha);
+                if(beta <= alpha){
+                    break;
+                }
+            }
+        }
+        else{
+            value = Integer.MAX_VALUE;
+            for(Board childBoard: boards){
+                int newValue = minimax(childBoard, depth -1, alpha, beta, true);
+                value = Math.min(newValue, value);
+
+                alpha = Math.min(value, beta);
+                if(beta <= alpha){
+                    break;
+                }
+            }
+        }
+
+        return value;
+
+    }
     private static int minimax(Board board, int depth, int alpha, int beta, boolean isMax){
+        searchCount++;
         int value;
         List<Board> boards = BoardGenerator.getBoards(board);
         if(depth == 0 || boards.size() == 0){//or node is terminal
@@ -51,10 +90,10 @@ class MiniMax{
             for(Board childBoard: boards){
                 int newValue = minimax(childBoard, depth -1, alpha, beta, true);
                 value = Math.min(newValue, value);
-                if(newValue >= alpha){//Should this be opposite
+                if(newValue >= alpha){
                     return value;
                 }
-                alpha = Math.min(value, alpha);
+                beta = Math.min(value, beta);
             }
         }
 
