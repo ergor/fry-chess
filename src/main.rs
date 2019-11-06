21e28f56cdb70b1cdfb15f709af7cd176223006e
stian.tosse@tripletex.no <stian.tosse@tripletex.no>
2019-11-06 16:33:48 +0100

    add some test code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+
+#[derive(Copy, Clone)]
+enum Color{
+    White,
+    Black
+}
+
+#[derive(Copy, Clone)]
+struct Board {
+    squares: [[i32; 8]; 8],
+    turn: Color
+}
+
+#[derive(Copy, Clone)]
+struct Point {
+    x:i32,
+    y:i32,
+    piece:i32
+}
+
+#[derive(Copy, Clone)]
+struct Board2 {
+    squares: [point; 64],
+    turn: Color
+}
+
+
+
 fn main() {
+    let board1 = Board{
+        squares : [
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+        ],
+        turn: Color::White
+    };
+
+
     println!("Hello, world!");
+
 }
