c04da92d91b6d4b3e0184efa0508d32ce9861ed0
stian.tosse@tripletex.no <stian.tosse@tripletex.no>
2019-11-08 10:36:17 +0100

    add king moves logic

--- src/generator.rs
+++ src/generator.rs
@@ -3,46 +3,115 @@ use chess_structs::*;
 use chess_structs::Piece::*;
 use chess_structs::Color::*;
 
+// idea: generate most likely board first, specific for black and white
+
+#[derive(Itrator)]
+struct MoveItr {
+    curr: Board,
+    next: Board,
+    pos: Index2D,
+    nr: i32
+}
+
+impl Iterator for MoveItr {
+    fn next() -> Option<Board> {
+
+    }
+}
 
 #[derive(Itrator)]
 struct KingItr {
     curr: Board,
     next: Board,
     pos: Index2D,
-    nr:i32
-
+    nr: i32
 }
+
  impl Iterator for KingItr {
      fn new(board: Board, pos: Index2D) -> KingItr {
         KingItr {
-            curr:board,
-            next:None,
+            curr: board,
+            next: None,
             pos,
-            nr:0
+            nr: 1
         }
      }
 
-     fn next(){
+     fn next() -> Option<Board> {
         match  nr{
             1 => {
-                let newPos = Index2D{
-                    x: pos.x + 1,
-                    y: pos.y
-                };
-                nr += 1;
-                doMove(curr, pos, newPos);
+                next_move(Index2D{ x: pos.x, y: pos.y + 1 }, 3)
             }
-            2 => board,
+            2 => {
+                next_move(Index2D{ x: pos.x + 1, y: pos.y + 1 }, 1)
+            }
+            3 => {
+                next_move(Index2D{ x: pos.x - 1, y: pos.y + 1 }, 1)
+            }
+            4 => {
+                next_move(Index2D{ x: pos.x, y: pos.y - 1 }, 3)
+            }
+            5 => {
+                next_move(Index2D{ x: pos.x + 1, y: pos.y - 1 }, 1)
+            }
+            6 => {
+                next_move(Index2D{ x: pos.x - 1, y: pos.y - 1}, 1)
+            }
+            7 => {
+                next_move(Index2D{ x: pos.x - 1, y: pos.y }, 1)
+            }
+            8 => {
+                next_move(Index2D{ x: pos.x + 1, y: pos.y }, 1)
+            }
+            _ => None
         }
      }
 
+     fn next_move(new_pos: Index2D, inc: i32) {
+         if is_out_of_board(new_pos) {
+             nr += inc;
+             next()
+         }
+         if is_legal_move(curr, pos, new_pos) {
+             nr += 1;
+             some(create_new_board(curr, pos, new_pos))
+         }
+         else {
+             nr += 1;
+             next()
+         }
+     }
  }
 
-
-pub fn doMove(board: Board, from: Index2d, to: Index2D) -> Board {
+pub fn create_new_board(board: Board, from: Index2D, to: Index2D) -> Board {
     let mut board = board;
-    board[to.y][to.x] = [from.y][from.x];
-    board[from.y][from.x] = Piece.Empty;
+    board.pieces[to.y][to.x] = [from.y][from.x];
+    board.pieces[from.y][from.x] = Piece.Empty;
+    board.turn = get_next_turn(board.turn);
+
     let board = board;
     board
+}
+
+pub fn is_legal_move(board: Board, from: Index2d, to: Index2D) -> bool {
+    board[to.y][to.x].kind == Kind.Empty || board[to.y][to.x].color == board.turn
+}
+
+pub fn is_out_of_board(new_pos:Index2D) -> bool {
+    let (x, y) = new_pos;
+
+    if x < 0 || x > 8 ||
+        y < 0 || y > 8 {
+        true
+    } else {
+        false
+    }
+}
+
+pub fn get_next_turn(color: Color) -> Color {
+    if color = Color.White {
+        Color.Black
+    }else {
+        Color.White
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,24 @@ use chess_structs::Piece::*;
 use chess_structs::Color::*;
 
 fn main() {
-    let board1 = Board{
-        squares : [
-            [Rook(Black),Knight(Black),Bishop(Black),Queen(Black),King(Black),Bishop(Black),Knight(Black),Rook(Black)],
-            [Pawn(Black); 8],
-            [Empty; 8],
-            [Empty; 8],
-            [Empty; 8],
-            [Empty; 8],
-            [Pawn(White); 8],
-            [Rook(White),Knight(White),Bishop(White),Queen(White),King(White),Bishop(White),Knight(White),Rook(White)],
-        ],
-        turn: Color::White,
-        en_passant: None,
-        white_kingside: false,
-        white_queenside: false,
-        black_kingside: false,
-        black_queenside: false,
-    };
+//    let board1 = Board{
+//        squares : [
+//            [Rook(Black),Knight(Black),Bishop(Black),Queen(Black),King(Black),Bishop(Black),Knight(Black),Rook(Black)],
+//            [Pawn(Black); 8],
+//            [Empty; 8],
+//            [Empty; 8],
+//            [Empty; 8],
+//            [Empty; 8],
+//            [Pawn(White); 8],
+//            [Rook(White),Knight(White),Bishop(White),Queen(White),King(White),Bishop(White),Knight(White),Rook(White)],
+//        ],
+//        turn: Color::White,
+//        en_passant: None,
+//        white_kingside: false,
+//        white_queenside: false,
+//        black_kingside: false,
+//        black_queenside: false,
+//    };
 
     println!("Hello, world!");
 
